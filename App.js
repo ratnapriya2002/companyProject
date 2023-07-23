4b7027d2483acf65da44a862735d9583ec5ea378
ratnapriya2002 <chamalapriya@gmail.com>
2023-07-23 23:28:25 +0530

    Leamigo

---
+++ App.js
@@ -0,0 +1,73 @@
+
+import React, { useState } from "react";
+import PointSelector from "./components/Travaling/PointSelector";
+import DateSelector from "./components/Travaling/DateSelector";
+import { vehiclesData } from "./data/Data";
+// import VehicleList from "./components/VehicleList";
+// import VehicleList from "./components/Travaling/VehilesList";
+import VehicleList from "./components/Travaling/VehilesList";
+import "./App.css";
+
+const App = () => {
+  const [pointA, setPointA] = useState("");
+  const [pointB, setPointB] = useState("");
+  const [selectedDate, setSelectedDate] = useState("");
+  const [errorMsg, SetErrorMsg] = useState("");
+  const [availableVehicles, setAvailableVehicles] = useState([]);
+
+  const handlePointASelection = (selectedPoint) => {
+    setPointA(selectedPoint);
+  };
+
+  const handlePointBSelection = (selectedPoint) => {
+    setPointB(selectedPoint);
+  };
+
+  const handleDateSelection = (selectedDate) => {
+    setSelectedDate(selectedDate);
+  };
+
+  const handleSearchButtonClick = async () => {
+    if (pointA === "" || pointB === "" || selectedDate === "") {
+      SetErrorMsg("Enter the fields");
+    } else {
+      SetErrorMsg("");
+      const filteredVehicles = vehiclesData.filter(
+        (vehicle) => vehicle.from === pointA && vehicle.to === pointB
+      );
+      setAvailableVehicles(filteredVehicles);
+    }
+  };
+
+  return (
+    <div className="bg-container">
+      <div className="container">
+        <PointSelector
+          selectedPoint={pointA}
+          onPointSelect={handlePointASelection}
+          label="From"
+        />
+        <PointSelector
+          selectedPoint={pointB}
+          onPointSelect={handlePointBSelection}
+          label="To"
+        />
+        <DateSelector
+          selectedDate={selectedDate}
+          onDateSelect={handleDateSelection}
+        />
+        <button onClick={handleSearchButtonClick}>Search</button>
+      </div>
+      {errorMsg !== "" && (
+        <p className="error-message">{errorMsg}</p>
+      )}
+      {availableVehicles.length !== 0 && (
+        <div className="vehicle-list-container" >
+          <VehicleList vehicles={availableVehicles}  />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ data/Data.js
@@ -0,0 +1,199 @@
+export const cities = ["Hyderabad", "Mumbai", "Chennai", "Bangalore", "Delhi"];
+
+// export const vehiclesData = [
+//   {
+//     id: 1,
+//     from: "Hyderabad",
+//     to: "Mumbai",
+//     vehiclesName: "SpiceJet",
+//   },
+//   {
+//     id: 2,
+//     from: "Hyderabad",
+//     to: "Chennai",
+//     vehiclesName: "Air India",
+//   },
+//   {
+//     id: 3,
+//     from: "Hyderabad",
+//     to: "Delhi",
+//     vehiclesName: "Air India",
+//   },
+//   {
+//     id: 4,
+//     from: "Hyderabad",
+//     to: "Bangalore",
+//     vehiclesName: "Air India",
+//   },
+//   {
+//     id: 5,
+//     from: "Bangalore",
+//     to: "Mumbai",
+//     vehiclesName: "SpiceJet",
+//   },
+//   {
+//     id: 6,
+//     from: "Bangalore",
+//     to: "Chennai",
+//     vehiclesName: "SpiceJet",
+//   },
+//   {
+//     id: 7,
+//     from: "Bangalore",
+//     to: "Delhi",
+//     vehiclesName: "Air India",
+//   },
+//   {
+//     id: 8,
+//     from: "Bangalore",
+//     to: "Hyderabad",
+//     vehiclesName: "SpiceJet",
+//   },
+// ];
+export const vehiclesData = [
+  {
+    id: 1,
+    from: "Hyderabad",
+    to: "Mumbai",
+    vehiclesName: "SpiceJet",
+  },
+  {
+    id: 2,
+    from: "Hyderabad",
+    to: "Chennai",
+    vehiclesName: "Air India",
+  },
+  {
+    id: 3,
+    from: "Hyderabad",
+    to: "Delhi",
+    vehiclesName: "Air India",
+  },
+  {
+    id: 4,
+    from: "Hyderabad",
+    to: "Bangalore",
+    vehiclesName: "Air India",
+  },
+  {
+    id: 5,
+    from: "Bangalore",
+    to: "Mumbai",
+    vehiclesName: "SpiceJet",
+  },
+  {
+    id: 6,
+    from: "Bangalore",
+    to: "Chennai",
+    vehiclesName: "SpiceJet",
+  },
+  {
+    id: 7,
+    from: "Bangalore",
+    to: "Delhi",
+    vehiclesName: "Air India",
+  },
+  {
+    id: 8,
+    from: "Bangalore",
+    to: "Hyderabad",
+    vehiclesName: "SpiceJet",
+  },
+  {
+    id: 9,
+    from: "Chennai",
+    to: "Delhi",
+    vehiclesName: "IndiGo",
+  },
+  {
+    id: 10,
+    from: "Chennai",
+    to: "Mumbai",
+    vehiclesName: "GoAir",
+  },
+  {
+    id: 11,
+    from: "Chennai",
+    to: "Hyderabad",
+    vehiclesName: "IndiGo",
+  },
+  {
+    id: 12,
+    from: "Chennai",
+    to: "Bangalore",
+    vehiclesName: "GoAir",
+  },
+  {
+    id: 13,
+    from: "Delhi",
+    to: "Mumbai",
+    vehiclesName: "Vistara",
+  },
+  {
+    id: 14,
+    from: "Delhi",
+    to: "Hyderabad",
+    vehiclesName: "IndiGo",
+  },
+  {
+    id: 15,
+    from: "Delhi",
+    to: "Chennai",
+    vehiclesName: "Vistara",
+  },
+  {
+    id: 16,
+    from: "Delhi",
+    to: "Bangalore",
+    vehiclesName: "AirAsia",
+  },
+  // Add 8 more vehicle names here
+  {
+    id: 17,
+    from: "Mumbai",
+    to: "Hyderabad",
+    vehiclesName: "Jet Airways",
+  },
+  {
+    id: 18,
+    from: "Mumbai",
+    to: "Chennai",
+    vehiclesName: "GoAir",
+  },
+  {
+    id: 19,
+    from: "Mumbai",
+    to: "Delhi",
+    vehiclesName: "Air India",
+  },
+  {
+    id: 20,
+    from: "Mumbai",
+    to: "Bangalore",
+    vehiclesName: "IndiGo",
+  },
+  {
+    id: 21,
+    from: "Chennai",
+    to: "Hyderabad",
+    vehiclesName: "AirAsia",
+  },
+  {
+    id: 22,
+    from: "Chennai",
+    to: "Mumbai",
+    vehiclesName: "Air India",
+  },
+  {
+    id: 23,
+    from: "Chennai",
+    to: "Delhi",
+    vehiclesName: "GoAir",
+  },
+  {
+    id: 24,
+    from: "Chennai",
+    to: "Bangalore",
+    vehiclesName: "SpiceJet",
+  },
+];
