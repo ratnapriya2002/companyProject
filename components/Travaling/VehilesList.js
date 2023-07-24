a073fa400af4d60def38bb1f2dc2fd131d3ff28f
ratnapriya2002 <chamalapriya@gmail.com>
2023-07-24 07:27:19 +0530

    update

--- components/Travaling/VehilesList.js
+++ components/Travaling/VehilesList.js
@@ -1,14 +1,33 @@
+// // VehicleList.jsx
+// import React from "react";
+
+// const VehicleList = ({ vehicles }) => {
+//   return (
+//     <div className="vacals-list" >
+//       <h1>Available Vehicles:</h1>
+//       <ul >
+//         {vehicles.map((vehicle) => (
+//           <li key={vehicle.id} className="list">{vehicle.vehiclesName}</li>
+          
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default VehicleList;
 // VehicleList.jsx
 import React from "react";
 
 const VehicleList = ({ vehicles }) => {
   return (
-    <div className="vacals-list" >
+    <div className="vacals-list">
       <h1>Available Vehicles:</h1>
-      <ul >
+      <ul>
         {vehicles.map((vehicle) => (
-          <li key={vehicle.id} className="list">{vehicle.vehiclesName}</li>
-          
+          <li key={vehicle.id} className="list">
+            {vehicle.vehiclesName}
+          </li>
         ))}
       </ul>
     </div>
